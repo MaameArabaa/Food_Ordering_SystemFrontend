ddde8daed0d150eddf22809007a768d8e8a2a6d7
Arabam <arabaamoah1612@gmail.com>
2024-12-08 15:23:42 +0000

    First commit

---
+++ scripts/Login.js
@@ -0,0 +1,17 @@
+const form = document.querySelector("form")
+const BACKEND_URL = "http://localhost:3000"
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    let fullname = document.querySelector(".fullname").value
+    let password = document.querySelector(".password").value
+    console.log(`Fullname: ${fullname} || password: ${password}`)
+    axios.post(`${BACKEND_URL}/login`,{fullname,password}).then(reponse=>{
+        if(reponse.data.message === "User login Successful"){
+            alert(reponse.data.message)
+            window.location.href = "allegies.html"
+        }
+    }).catch(err=>{
+        console.log(err)
+    })
+})
